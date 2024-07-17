c8d3ffbb3347438802b54952f0052163e1c66b89
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-17 02:11:29 -0400

    Update export

--- flake.nix
+++ flake.nix
@@ -1,13 +1,10 @@
 {
   description = "Nix latex packaging utilities";
 
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs = { };
 
   outputs =
-    { }:
+    { self, ... }:
     {
       buildLatexDocument = import ./package.nix;
     };
