9e0b4df8986ca7799b307275c4f7efddbcb4f235
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-17 12:25:08 -0400

    Change document name

--- package.nix
+++ package.nix
@@ -6,13 +6,14 @@
   texpkgs ? { },
   builder ? "lualatex",
   lastModified ? 0,
+  document ? "document",
   ...
 }:
 let
   tex = texlive.combine { inherit (pkgs.texlive) scheme-minimal latex-bin latexmk; } // texpkgs;
 in
 pkgs.stdenvNoCC.mkDerivation {
-  inherit name;
+  inherit name src;
   dontUnpack = true;
   buildPhase = ''
     mkdir -p .cache/texmf-var
@@ -24,11 +25,10 @@ pkgs.stdenvNoCC.mkDerivation {
       -${builder} \
       -pretex='\pdfvariable suppressoptionalinfo 512\relax' \
       -jobname=document \
-      ${src}
+      ${document}
   '';
   installPhase = ''
     mkdir -p $out
     cp document.pdf $out/${name}.pdf
   '';
 }
-
