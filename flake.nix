bc22d3bd2f5822c00475f7d5f01d20f231ebf54e
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-17 02:01:03 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Nix latex packaging utilities";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          default = (pkgs.callPackage ./package.nix);
+        };
+      }
+    );
+}
